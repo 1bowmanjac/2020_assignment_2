c654d328fb4ffe70dd9e6829f1ab8786b4b19a6c
Jack Bowman <1bowmanjac@gmail.com>
2021-03-30 21:32:29 -0400

    fully functional command line wise

--- src/sample/Client.java
+++ src/sample/Client.java
@@ -1,30 +1,108 @@
 package sample;
 
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Scanner;
 import java.net.Socket;
-import java.io.IOException;
 
 /**
- * A command line client for the date server. Requires the IP address of the
- * server as the sole argument. Exits after printing the response.
+ * Client class:
+ * I think you can run this from anywhere
+ * I mostly ran it from jack@Laptop:~/Documents/Programs/Java/Assignment_2/src/sample
+ * look at main method for example on how to use
  */
 public class Client {
-    public static void main(String[] args) throws IOException {
-
+    DataInputStream in;
+    DataOutputStream out;
+    public Client(Socket socket) throws IOException {
+        this.out = new DataOutputStream(socket.getOutputStream());
+        this.in = new DataInputStream(socket.getInputStream());
+    }
+    public static void main(String[] args) throws IOException, InterruptedException {
+        //create socket and client
         var socket = new Socket("localhost", 25505);
-        var in = new Scanner(socket.getInputStream());
-        var out = new PrintWriter(socket.getOutputStream(), true);
+        Client client = new Client(socket);
+        /**
+         * command examples that worked for me are commented above their if statement
+         */
+        //command: java Client.java DIR
+        if(args[0].equals("DIR")){
+            System.out.println("Available Files: ");
+            System.out.print(client.DIR());
 
-        String command = "";
-        for (String item:
-             args) {
-            command += item + "\n";
-        }
-        out.println(command);
-        while (in.hasNextLine()){
-            System.out.println("Server response: " + in.nextLine());
+        //command: java Client.java UPLOAD ~jack/Downloads/WordCounter2.zip
+        } if(args[0].equals("UPLOAD")){
+            File file = new File(args[1]);
+            client.UPLOAD(file);
+
+        //command: java Client.java DOWNLOAD WordCounter2.zip ~jack/Downloads/
+        } if(args[0].equals("DOWNLOAD")){
+            client.DOWNLOAD(args[1],args[2]);
         }
 
+    }
+
+    /**
+     * returns a string of all available files from the server
+     * @return String of filenames seperated by \n
+     * @throws IOException
+     */
+    public String DIR() throws IOException {
+        out.writeUTF("DIR");
+        String fileList = in.readUTF();
+        return fileList;
+    }
+
+    /**
+     * Uploads a file to the server
+     * @param file File that you want to be uploaded
+     * @return String File Upload Confirmation
+     * @throws IOException
+     */
+    public String UPLOAD(File file) throws IOException {
+        out.writeUTF("UPLOAD\n" + file.getName());
+        System.out.println(in.readUTF());
+
+        //this should read from file in bite (ha) sized pieces
+        //rather than loading the entire file to ram
+        FileInputStream fileIn = new FileInputStream(file);
+        int bytes=0;
+        out.writeLong(file.length());
+        byte[] buffer = new byte[4*1024];
+        while ((bytes=fileIn.read(buffer))!=-1){
+            out.write(buffer,0,bytes);
+            out.flush();
+        }
+        return in.readUTF();
+    }
+
+    /**
+     * Downloads selected file from server
+     * @param filename name of file to be downloaded
+     * @param downloadLocation path of where you want it saved e.g  ~jack/Downloads/
+     * @return String of downloaded file and confirmation
+     * @throws IOException
+     */
+    public String DOWNLOAD(String filename,String downloadLocation) throws IOException {
+
+        out.writeUTF("DOWNLOAD\n" + filename);
+        out.flush();
+        //sets filepath
+        System.out.println(downloadLocation);
+        File dir = new File (downloadLocation);
+        File file = new File (dir, filename);
+
+
+        //this should write to file in bite (ha) sized pieces
+        //rather than loading the entire file to ram
+        long size = in.readLong();
+        FileOutputStream fileOut = new FileOutputStream(file);
+        byte[] buffer = new byte[4*1024];
+        int bytes = 0;
+        while (size > 0 && (bytes = in.read(buffer, 0, (int)Math.min(buffer.length, size))) != -1) {
+            fileOut.write(buffer,0,bytes);
+            size -= bytes;
+        }fileOut.close();
+        out.writeUTF("successfully downloaded " + filename);
+        return "downloaded " + filename;
     }
 }
--- src/sample/Server.java
+++ src/sample/Server.java
@@ -1,22 +1,52 @@
 package sample;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.concurrent.Executors;
 
+/**
+ * YOU MUST RUN SERVER FROM ASSIGNMENT 2 FOLDER
+ * E.G: jack@Laptop:~/Documents/Programs/Java/Assignment_2$ java src/sample/Server.java
+ */
 public class Server {
-    private static ArrayList<String> fileList = new ArrayList<>();
-    public static ArrayList<String> getFileList(){
+    /**
+     *
+     * @return string of files seperated by \n
+     */
+    public static String getFileList(){
+        String fileList = "";
+        try {
+            File myObj = new File("filenames.txt");
+            Scanner reader = new Scanner(myObj);
+            while (reader.hasNextLine()) {
+                fileList  += reader.nextLine() + "\n";
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
         return fileList;
     }
+
+    /**
+     * adds file to list of files
+     * @param newFile string name of file to be added
+     */
     public static void addToFileList(String newFile){
-        fileList.add(newFile);
+        try(FileWriter fw = new FileWriter("filenames.txt", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter out = new PrintWriter(bw))
+        {
+            out.println(newFile);
+        } catch (IOException e) {
+            //exception handling left as an exercise for the reader
+        }
     }
     public static void main(String[] args) throws Exception {
-        addToFileList("first File");
+
         try (var listener = new ServerSocket(25505)) {
             System.out.println("The server is running...");
             var pool = Executors.newFixedThreadPool(20);
@@ -28,7 +58,7 @@ public class Server {
 
 
 }
-
+//one instance per connection
 class instance implements Runnable {
     private Socket socket;
 
@@ -39,35 +69,83 @@ class instance implements Runnable {
     @Override
     public void run() {
         System.out.println("Connected: " + socket);
+        DataInputStream in = null;
+        DataOutputStream out = null;
         try {
-            var in = new Scanner(socket.getInputStream());
-            var out = new PrintWriter(socket.getOutputStream(), true);
-            while (in.hasNextLine()) {
-                String arg = in.nextLine();
-                System.out.println(arg);
-                if(arg.equals("DIR")){
-                    for (int i=0;i < Server.getFileList().size();i++){
-                        out.println(Server.getFileList().get(i));
-                    }
-                    break;
-                } if(arg.equals("UPLOAD")){
-                    Server.addToFileList(in.nextLine());
-                    break;
-                } if(arg.equals("DOWNLOAD")){
-                    String fileName = in.nextLine();
-                    for(int i = 0; i < Server.getFileList().size();i++){
-                        if(Server.getFileList().get(i).equals(fileName)){
-                            out.println(fileName);
-                            break;
-                        }
-                    }
-                    break;
-                } else {
-                    out.println("not a recognized command");
+             in = new DataInputStream(socket.getInputStream());
+             out = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+                /**
+                 * format of a command is one argument per line
+                 * first line is always the command eg DIR, UPLOAD, DOWNLOAD
+                 * the following is what the client sends:
+                 *      "UPLOAD" + "\n" + "filename"
+                 *      "DIR"
+                 *      "DOWNLOAD" + "\n" + "filename"
+                 */
+                String args[] = in.readUTF().split("\\r?\\n");
+                System.out.println(args[0]);
+
+                if(args[0].equals("DIR")){
+                    out.writeUTF(Server.getFileList());
                 }
-            }
+                else if(args[0].equals("UPLOAD")){
+                    String filename = args[1];
+                    out.writeUTF("Got Filename");
+                    out.flush();
+                    //sets filepath
+                    File dir = new File ("files");
+                    File file = new File (dir, filename);
+                    System.out.println("filepath = " + file.getAbsolutePath());
+
+
+                    //this should write to file in bite (ha) sized pieces
+                    //rather than loading the entire file to ram
+                    long size = in.readLong();
+                    FileOutputStream fileOut = new FileOutputStream(file);
+                    byte[] buffer = new byte[4*1024];
+                    int bytes = 0;
+                    while (size > 0 && (bytes = in.read(buffer, 0, (int)Math.min(buffer.length, size))) != -1) {
+                        fileOut.write(buffer,0,bytes);
+                        size -= bytes;
+                    }fileOut.close();
+
+                    //update fileList
+                    Server.addToFileList(filename);
+                    out.writeUTF("successfully uploaded " + filename);
+
+                }
+                else if(args[0].equals("DOWNLOAD")){
+                    String filename = args[1];
+                        File dir = new File ("files");
+                        File file = new File (dir, filename);
+                    System.out.println("filepath = " + file.getAbsolutePath());
+
+                    //this should read from file in bite (ha) sized pieces
+                    //rather than loading the entire file to ram
+                    FileInputStream fileIn = new FileInputStream(file);
+                    int bytes=0;
+                    out.writeLong(file.length());
+                    byte[] buffer = new byte[4*1024];
+                    while ((bytes=fileIn.read(buffer))!=-1){
+                        out.write(buffer,0,bytes);
+                        out.flush();
+                    }
+                    System.out.println(in.readUTF());
+                } else {
+                    out.writeUTF("not a recognized command");
+                }
+// messy error catches do not touch
         } catch (Exception e) {
-            System.out.println("Error:" + socket);
+            System.out.println("Error:" + e);
+            try {
+                out.writeUTF(e.toString());
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
         } finally {
             try {
                 socket.close();
@@ -76,8 +154,6 @@ class instance implements Runnable {
             System.out.println("Closed: " + socket);
         }
     }
-    private void addToFileList(String newFile){
 
-    }
 }
 
